441aeda9b9d025cc698997842a0adb956f07cd89
unknown <rsimon@W7P-RIU002.CIP.CGIARAD.ORG>
2015-02-17 10:51:31 -0500

    Moving to main site

---
+++ app/cateagories.js
@@ -0,0 +1,48 @@
+// Some sample categorized data. This data is in-memory
+// for demonstration purposes, but could be loaded dynamically
+// via ajax.
+var categoryData = {
+	clones: {
+		name: "Potato Varieties",
+		description: "The Potato Catalogue contains information of 70 potato varieties selected from breeding material provided by the International Potato Center (CIP) and released by national potato program.",
+		items: [
+			{
+				name: "CIP374080.1"
+			},
+			{
+				name: "CIP379666.501"
+			},
+			{
+				name: "CIP386040.9"
+			}
+		]
+	},
+	countries: {
+		name: "Countries of selections",
+		description: "",
+		items: [
+			{
+				name: "Peru"
+			},
+			{
+				name: "Bangladesh"
+			},
+			{
+				name: "Costa Rica"
+			}
+		]
+	},
+	reactions: {
+		name: "Reactions to diseases",
+		description: "",
+		items: [
+			{
+				name: "medium"
+			},
+			{
+				name: "moderately"
+			}
+		]
+	}
+};
+
---
+++ app/items.js
@@ -0,0 +1,88 @@
+var itemData = {
+	'CIP374080.1': {
+		name: "CIP374080.1",
+		description: "The variety has a plant with medium vegetative period with a semi-erect growth habit with intense lilac flowers with white acumen in both surfaces; it is highly appreciated in the Altiplano of the Andes for their rounded tubers with white-cream flesh and shallow eyes. The variety was released in Peru in 1988.",
+		attr: [
+		  {
+				name: "Tuber image",
+				valu: "tuber/374080.1.jpg"
+			},
+			{
+				name: "Plant image",
+				valu: "plant/374080.1.jpg"
+			},
+			{
+				name: "Sprout image",
+				valu: "sprout/374080.1.jpg"
+			},
+		  {
+				name: "Country of selection",
+				valu: "Peru"
+			},
+			{
+				name: "Tuber flesh predominant color",
+				valu: "cream"
+			},
+			{
+				name: "Tuber skin predominant color",
+				valu: "white-cream"
+			},
+			{
+				name: "Tuber shape",
+				valu: "rounded"
+			},
+			{
+				name: "Late Blight reaction",
+				valu: "moderately resistant"
+			},
+			{
+				name: "Growing period",
+				valu: "medium"
+			}
+		]
+	},
+'CIP379666.501': {
+		name: "CIP379666.501",
+		description: "My favorite CIP clone.",
+		attr: [
+		  {
+				name: "Country of selection",
+				valu: "India"
+			},
+			{
+				name: "Tuber flesh color",
+				valu: "pink"
+			},
+			{
+				name: "Tuber skin color",
+				valu: "red"
+			},
+			{
+				name: "Resistance to Late Blight",
+				valu: "high"
+			}
+		]
+	},
+'CIP386040.9': {
+		name: "CIP386040.9",
+		description: "My favorite CIP clone.",
+		attr: [
+		  {
+				name: "Country of selection",
+				valu: "India"
+			},
+			{
+				name: "Tuber flesh color",
+				valu: "pink"
+			},
+			{
+				name: "Tuber skin color",
+				valu: "red"
+			},
+			{
+				name: "Resistance to Late Blight",
+				valu: "high"
+			}
+		]
+	}
+}
