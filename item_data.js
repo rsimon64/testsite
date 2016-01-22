4214bbacb6980e95ef3656cb13fa56e95c2739ed
c5sire <rsimon64@gmail.com>
2016-01-22 05:06:07 -0500

    Cleaning up

---
+++ item_data.js
@@ -0,0 +1,172 @@
+
+var itemData = {
+	'CIP374080.1': {
+		name: "CIP374080.1",
+		description: "The variety has a plant with medium vegetative period with a semi-erect growth habit with intense lilac flowers with white acumen in both surfaces; it is highly appreciated in the Altiplano of the Andes for their rounded tubers with white-cream flesh and shallow eyes; there can be process as 'chuño' and for table potato and chips (crisps). The variety was released in Peru in 1988.",
+		attr: [
+		  {
+				name: "Tuber image",
+				valu: "tuber/374080.1.jpg"
+			},
+			{
+				name: "Plant image",
+				valu: "plant/374080.1.jpg"
+			},
+			{
+				name: "Sprout image",
+				valu: "sprout/374080.1.jpg"
+			},
+		  {
+				name: "Country of selection",
+				valu: "Peru"
+			},
+			{
+				name: "Tuber flesh predominant color",
+				valu: "cream"
+			},
+			{
+				name: "Tuber skin predominant color",
+				valu: "white-cream"
+			},
+			{
+				name: "Tuber shape",
+				valu: "rounded"
+			},
+			{
+				name: "Late Blight reaction",
+				valu: "moderately resistant"
+			},
+			{
+				name: "Growing period",
+				valu: "medium"
+			}
+		]
+	},
+'CIP379666.501': {
+		name: "CIP379666.501",
+		description: "Is moderately resistant to late blight. It has a medium growing period, with decumbent plant habit and pale white flowers. Tubers have white flesh with an oblong shape. Heera is a table potato.",
+		attr: [
+		  {
+				name: "Tuber image",
+				valu: "tuber/379666.501.jpg"
+			},
+			{
+				name: "Plant image",
+				valu: "plant/379666.501.jpg"
+			},
+			{
+				name: "Sprout image",
+				valu: "sprout/379666.501.jpg"
+			},
+		  {
+				name: "Country of selection",
+				valu: "Bangladesh"
+			},
+			{
+				name: "Tuber flesh color",
+				valu: "white"
+			},
+			{
+				name: "Tuber skin color",
+				valu: "white-cream"
+			},
+			{
+				name: "Resistance to Late Blight",
+				valu: "moderately-resistant"
+			}
+		]
+	},
+'CIP386040.9': {
+		name: "CIP386040.9",
+		description: "Is a variety with moderately resistant to late blight. The plant has a medium vegetative period with decumbent plant habit; adapted to short days in cool tropical climate, the flowers are lilac with white acumen abaxial surface. The tubers are oblong with white flesh can be use as table potato. The variety was released in Guatemala as ICTA- Mercedes in 1991 and in Costa Rica as Birris in 1996.",
+		attr: [
+		  {
+				name: "Tuber image",
+				valu: "tuber/386040.9.jpg"
+			},
+			{
+				name: "Plant image",
+				valu: "plant/386040.9.jpg"
+			},
+			{
+				name: "Sprout image",
+				valu: "sprout/386040.9.jpg"
+			},
+		  {
+				name: "Country of selection",
+				valu: "Costa Rica"
+			},
+			{
+				name: "Tuber flesh color",
+				valu: "white"
+			},
+			{
+				name: "Tuber skin color",
+				valu: "white-cream"
+			},
+			{
+				name: "_RLB",
+				valu: "_MR"
+			}
+		]
+	},
+'CIP300046.22': {
+		name: "CIP300046.22",
+		description: "_LTVR",
+		attr: [
+		  {
+				name: "Tuber image",
+				valu: "tuber/300046.22.jpg"
+			},
+			{
+				name: "Plant image",
+				valu: "plant/300046.22.jpg"
+			},
+			
+			{
+				name: "_TBFLSH",
+				valu: "_CR"
+			},
+			{
+				name: "_TBSKN",
+				valu: "_WCR"
+			},
+			{
+				name: "_RLB",
+				valu: "_R"
+			}
+		]
+	},
+'CIP300048.12': {
+		name: "CIP300048.12",
+		description: "_LTVR",
+		attr: [
+		  {
+				name: "Tuber image",
+				valu: "tuber/300048.12.jpg"
+			},
+			{
+				name: "Plant image",
+				valu: "plant/300048.12.jpg"
+			},
+			{
+				name: "Sprout image",
+				valu: "sprout/300048.12.jpg"
+			},
+		  
+			{
+				name: "_TBFLSH",
+				valu: "_CR"
+			},
+			{
+				name: "_TBSKN",
+				valu: "_WCR"
+			},
+			{
+				name: "_RLB",
+				valu: "_HS"
+			}
+		]
+	}
+};
+
